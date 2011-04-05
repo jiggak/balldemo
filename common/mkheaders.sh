f088641dd6999ed1b65b76e95784ac4d3d8d5da7
josh <kropf@echoworx.com>
2011-04-05 13:50:06 -0400

    generate ball texture asset header

---
+++ common/mkheaders.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+path2cname() {
+  f=$(basename $1)
+  echo ${f/./_}
+}
+
+if [ ! -x bin2c ]; then
+  gcc -o bin2c bin2c.c
+fi
+
+SRC="sprite.glslv sprite.glslf"
+HEADER="shaders.h"
+
+rm -f ${HEADER}
+for f in ${SRC}; do
+  name=$(path2cname $f)
+  echo -e "const char ${name}[] =" >>${HEADER}
+  ./bin2c <${f} >>${HEADER}
+  echo -e '\t"\\x00";' >>${HEADER}
+done
+
+SRC="../assets/ball.tga"
+HEADER="assets.h"
+
+rm -f ${HEADER}
+for f in ${SRC}; do
+  ./bin2c $(path2cname $f) <${f} >>${HEADER}
+done
